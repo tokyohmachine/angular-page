ad1bb63ad407c3c06452d201c42acba23a06d591
Daniela <danielamedinna20@gmail.com>
2023-10-09 16:57:12 -0300

    media query created

--- src/app/components/black-side/black-side.component.ts
+++ src/app/components/black-side/black-side.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-black-side',
   templateUrl: './black-side.component.html',
   styleUrls: ['./black-side.component.css', './black-side.responsive.component.css']
 })
-export class BlackSideComponent {
+export class BlackSideComponent implements OnInit {
+  titleRight:string = "ORDER.NOW"
+  h3Sub:string = "OVER 200 AWARDS"
+  h3Title:string = "SELECT.YOUR.PLATFORM:"
+  paraGraph:string = "Cyberpunk 2077 is an open-world, action-adventure storu set in Night City, a megalopolis obsessed with power, glamour and body modifiction."
+  constructor() {}
+  buttonLink:string = "PRE.ORDER.NOW"
+  endPara:string = "COMING DECEMBER 10TH, 2020"
+  ngOnInit(): void {}
 
 }
--- src/app/components/yellow-side/yellow-side.component.ts
+++ src/app/components/yellow-side/yellow-side.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-yellow-side',
   templateUrl: './yellow-side.component.html',
   styleUrls: ['./yellow-side.component.css', './yellow-side.responsive.component.css']
 })
-export class YellowSideComponent {
+
+export class YellowSideComponent implements OnInit{
+  @Input()
+  imageCover:string = ""
+  iconCover:string = "/assets/more.png"
+  subTitle:string = "BEST OPEN-WORLD GAME"
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
